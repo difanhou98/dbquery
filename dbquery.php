e69b6e35163e70d12045e9a6d939f7a6a9e78d45
Difan Hou <difanhou98@gmail.com>
2022-10-26 23:51:57 -0700

    query selectors finished

--- dbquery.php
+++ dbquery.php
@@ -3,23 +3,32 @@
     require("db.php");
 
     $connection = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+    
 
     // test connection
     if(mysqli_connect_errno()) {
         die(mysqli_connect_error()) ;
     }
-
+    // specify the query and receive results
     $order_number_query = "SELECT orderNumber FROM orders";
     $order_number_result = mysqli_query($connection, $order_number_query);
+    // test if query fails
     if (!$order_number_result){
         die("query failed");
     }
 
-    
+    // test the length of returned data, store data in each row in an array
     if (mysqli_num_rows($order_number_result) != 0){
         while ($row = mysqli_fetch_assoc($order_number_result))
         $order_number[] = $row['orderNumber'];
     }
+
+    
+
+    
+
+    
+
 
 
     
@@ -38,17 +47,24 @@
         <div class="order-param">
             <p>Select Order Parameters</p>
             <!-- set method and action to submit forms to the same page -->
-            <form action="/dbquery.php" method="POST">
+            <form action="" method="POST">
                 <label for="order-id">Order Number: </label>
-                
+                <select id="order-id" name="order_id">'
+                <!-- iterate through $order_number[] to display all order number in the drop-down menu -->
                 <?php 
-                    echo '<select id="order-id" name="order_id">';
                     foreach($order_number as $orderid){
-                        echo '<option value='.$orderid.'>'.$orderid.'</option>';
+                        $selected = (strcmp($orderid, $_POST['order_id']) == 0 
+                        || strcmp("N/A", $_POST['order_id']) == 0 
+                        ? "selected" 
+                        : "");
+                        echo '<option value=' . $orderid . " " . $selected . '>' . $orderid . '</option>';
                     }
+                    echo '<option value=' . "N/A" . " " . $selected . '>' . "N/A" . '</option>';
+                    // echo '<option value="" selected="selected"></option>';
+                    echo '</select>';
                 ?>
-                <select name="" id=""></select>
                 <label for="order-id"> or</label>
+                
                 <p>Order Date (YYYY-MM-DD)</p>
                 <!-- Start Date -->
                 <label for="start-date">From: </label>
@@ -57,35 +73,141 @@
                 <label for="end-date">to: </label>
                 <input type="date" id="end-date" name="end_date">
                 <input type="submit" value="Search" name="submit">
+
+                <p>Columns to Display </p>
+                <!-- Order Number -->
+                <label for="order-number">Order Number</label>
+                <input type="checkbox" id="order-number" value="orders.orderNnumber" name="display_list[]"  
+                <?php 
+                    if (!empty($_POST['display_list']) && in_array("orders.orderNnumber", $_POST['display_list']))
+                    echo 'checked="checked"';
+                ?>>
+                <!-- Order Date -->
+                <label for="order-date">Order Date</label>
+                <input type="checkbox" id="order-date" value="orders.orderDate" name="display_list[]"
+                <?php 
+                    if (!empty($_POST['display_list']) && in_array("orders.orderDate", $_POST['display_list']))
+                    echo 'checked="checked"';
+                ?>>
+                <!-- Shipped Date -->
+                <label for="shipped-date">Shipped Date</label>
+                <input type="checkbox" id="shipped_date" value="orders.shippedDate" name="display_list[]"
+                <?php 
+                    if (!empty($_POST['display_list']) && in_array("orders.shippedDate", $_POST['display_list']))
+                    echo 'checked="checked"';
+                ?>>
+                <!-- Product Name -->
+                <label for="product-name">Product Name</label>
+                <input type="checkbox" id="product-name" value="products.productName" name="display_list[]"
+                <?php 
+                    if (!empty($_POST['display_list']) && in_array("products.productName", $_POST['display_list']))
+                    echo 'checked="checked"';
+                ?>>
+                <!-- Product Description -->
+                <label for="product-description">Product Description</label>
+                <input type="checkbox" id="product-description" value="products.productDescription" name="display_list[]"
+                <?php 
+                    if (!empty($_POST['display_list']) && in_array("products.productDescription", $_POST['display_list']))
+                    echo 'checked="checked"';
+                ?>>
+                <!-- Quantity Ordered -->
+                <label for="quantity-ordered">Quantity Ordered</label>
+                <input type="checkbox" id="quantity-ordered" value="orderdetails.quantityOrdered" name="display_list[]"
+                <?php 
+                    if (!empty($_POST['display_list']) && in_array("orderdetails.quantityOrdered", $_POST['display_list']))
+                    echo 'checked="checked"';
+                ?>>
+                <!-- Price Each -->
+                <label for="price-each">Price Each</label>
+                <input type="checkbox" id="price-each" value="orderdetails.priceEach" name="display_list[]"
+                <?php 
+                    if (!empty($_POST['display_list']) && in_array("orderdetails.priceEach", $_POST['display_list']))
+                    echo 'checked="checked"';
+                ?>>
             </form>
         </div>
         <!-- column sidplay section -->
-        <div class="column-display">
-        <form action="/dbquery.php" method="POST">
-            <p>Columns to Display </p>
-            <!-- Order Number -->
-            <label for="order-number">Order Number</label>
-            <input type="checkbox" id="order-number" value="orderNnumber" name="display_list[]">
-            <!-- Order Date -->
-            <label for="order-number">Order Date</label>
-            <input type="checkbox" id="order-number" value="orderDate" name="display_list[]">
-            <!-- Shipped Date -->
-            <label for="order-number">Shipped Date</label>
-            <input type="checkbox" id="order-number" value="shippedDate" name="display_list[]">
-            <!-- Product Name -->
-            <label for="order-number">Product Name</label>
-            <input type="checkbox" id="order-number" value="productName" name="display_list[]">
-            <!-- Product Description -->
-            <label for="order-number">Product Description</label>
-            <input type="checkbox" id="order-number" value="productDescription" name="display_list[]">
-            <!-- Quantity Ordered -->
-            <label for="order-number">Quantity Ordered</label>
-            <input type="checkbox" id="order-number" value="quantityOrdered" name="display_list[]">
-            <!-- Price Each -->
-            <label for="order-number">Price Each</label>
-            <input type="checkbox" id="order-number" value="priceEach" name="display_list[]">
-        </form>
-        </div>
     </div>
 </body>
 </html>
+<?php
+if (isset($_POST['submit'])){
+    if (!empty($_POST['display_list'])){
+        $selected_column = implode(",", $_POST['display_list']);
+        $selected_join = "";
+        $selected_query = "";
+        echo "<p>display list set</p>";
+        if (str_contains($selected_column, 'orderdetails') && str_contains($selected_column, 'products')){
+            $selected_join = " INNER JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber INNER JOIN products ON orderdetails.productCode = products.productCode";
+        }
+        else if (str_contains($selected_column, 'orderdetails')) {
+            $selected_join = " INNER JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber";
+        }
+        else if (str_contains($selected_column, 'products')) {
+            $selected_join = " INNER JOIN products ON orderdetails.productCode = products.productCode";
+        }
+        if (isset($_POST['order_id']) && !empty($_POST['order_id'])){
+        
+        echo '<p>orderid set!</p>';
+        }
+        else {
+            echo '<p>orderid not set!</p>';
+        }
+       
+        
+        
+        if (isset($_POST['order_id'])){
+            $selected_query = "SELECT ".$selected_column. " FROM ". "orders" . $selected_join. " WHERE orders.orderNumber = " . $_POST['order_id'];
+            
+            echo $selected_query;
+           
+            $selected_result = mysqli_query($connection, $selected_query);
+            if (!$selected_result){
+                die("Database query failed.");
+            }
+
+            if(mysqli_num_rows($selected_result) != 0){
+                
+                $columns = [];
+                $trimColumns = [];
+                echo    "<table>
+                        <tr>";
+                for ($x = 0; $x < count($_POST['display_list']); $x++){
+                    array_push($columns,$_POST['display_list'][$x]);
+                    echo "<td>" . $_POST['display_list'][$x] . "</td>";
+                }
+                echo "</tr><tr>";
+                //echo $columns;
+                foreach ($column as $columnItem){
+                    $pos = strpos($columnItem, ".");
+                    array_push($trimColumns,substr($columnItem, $pos));
+        
+                }
+                //echo $trimColumns;
+                while($row= mysqli_fetch_assoc($selected_result)) {
+                    for ($x = 0; $x < count($columns); $x++){
+                        echo "<td>" . $row['orderNumber'] . "</td>";
+                    }
+                }
+                echo "  </tr>
+                        </table>";             
+            }
+            else {
+                echo "no entry found.";
+            }
+            // start here, trim the column
+        }
+        else if (isset($_POST['start_date']) && isset($_POST['end_date'])){
+
+        }
+        else {
+            echo 'Please specify order number or order date to start the query process!';
+            exit;
+        }
+    }
+    else {
+        echo "<p>display list not set</p>";
+    }
+    
+} 
+?>
